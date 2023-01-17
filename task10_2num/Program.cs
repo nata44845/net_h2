f6d70673a227b6f7cc4006a202c86190295cc20a
nata44845 <krppost@yandex.ru>
2023-01-17 22:35:34 +0700

    homework

--- task10_2num/Program.cs
+++ task10_2num/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n>999 || n<100)
+{
+    Console.WriteLine($"Число {n} не трехзначное");
+}
+else
+{
+    int num=n%100/10;
+    Console.WriteLine($"Вторая цифра числа {n} равна "+num);
+}
--- task15_weekend/Program.cs
+++ task15_weekend/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n ==6 || n==7) Console.WriteLine("да");
+else Console.WriteLine("нет");
